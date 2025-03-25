6c907cdf7bf5efc1a922f8cec451a062ed61ffab
Abhi Bagai <abagai@my.bcit.ca>
2025-03-25 13:03:20 -0700

    add connection check log

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const dbConfigLocal = {
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
+	port: process.env.DB_PORT,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	multipleStatements: false,
--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 const dbConfigLocal = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
+console.log("Sequelize Connection String: ", dbConfigLocal);
 module.exports = dbConfigLocal;
